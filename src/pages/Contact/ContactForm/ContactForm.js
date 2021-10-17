89885bbb286475ff6ed77f3914f2917ff1448137
ado durak <76266634+Alginest@users.noreply.github.com>
2021-10-17 15:44:33 +0200

    Add files via upload

---
+++ src/pages/Contact/ContactForm/ContactForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import { Box } from '@mui/system'
+
+const useStyles = makeStyles((theme) => ({}))
+
+const CategoryBar = () => {
+  const classes = useStyles()
+  return <Box></Box>
+}
+
+export default CategoryBar
